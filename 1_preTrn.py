2eae61015661056785b3d42cfcd9fdd3e087ebfb
RohitDhankar <dhankar.rohit@gmail.com>
2020-08-28 23:58:28 +0530

    torchvision_models

---
+++ 1_preTrn.py
@@ -0,0 +1,21 @@
+# Pre Trained Networks 
+import torch 
+from torchvision import models
+print(dir(models))
+"""
+['AlexNet', 'DenseNet', 'GoogLeNet', 'GoogLeNetOutputs', 'Inception3', 
+'InceptionOutputs', 'MNASNet', 'MobileNetV2', 'ResNet', 'ShuffleNetV2', 
+'SqueezeNet', 'VGG', '_GoogLeNetOutputs', '_InceptionOutputs', 
+'__builtins__', '__cached__', '__doc__', '__file__', '__loader__', 
+'__name__', '__package__', '__path__', '__spec__', '_utils', 'alexnet', 
+'densenet', 'densenet121', 'densenet161', 'densenet169', 'densenet201', 
+'detection', 'googlenet', 'inception', 'inception_v3', 'mnasnet', 
+'mnasnet0_5', 'mnasnet0_75', 'mnasnet1_0', 'mnasnet1_3', 'mobilenet', 
+'mobilenet_v2', 'quantization', 'resnet', 'resnet101', 'resnet152', 
+'resnet18', 'resnet34', 'resnet50', 'resnext101_32x8d', 'resnext50_32x4d', 
+'segmentation', 'shufflenet_v2_x0_5', 'shufflenet_v2_x1_0', 
+'shufflenet_v2_x1_5', 'shufflenet_v2_x2_0', 'shufflenetv2', 'squeezenet', 
+'squeezenet1_0', 'squeezenet1_1', 'utils', 'vgg', 'vgg11', 'vgg11_bn', 
+'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn', 'vgg19', 'vgg19_bn', 'video', 
+'wide_resnet101_2', 'wide_resnet50_2']
+"""
---
+++ ownCodeLogs.md
@@ -0,0 +1,42 @@
+
+##### Check all systems GO !!!
+
+"""
+(pytorch_venv) dhankar@dhankar-1:~/temp/pytorch/PyTorch_1$ python
+Python 3.8.5 (default, Aug  5 2020, 08:36:46) 
+[GCC 7.3.0] :: Anaconda, Inc. on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 
+>>> from __future__ import print_function
+>>> import torch
+>>> x = torch.rand(5, 3)
+>>> print(x)
+tensor([[0.1965, 0.8001, 0.6781],
+        [0.9286, 0.7364, 0.2602],
+        [0.1383, 0.6738, 0.3003],
+        [0.6685, 0.4664, 0.9403],
+        [0.6227, 0.5124, 0.4035]])
+>>> 
+>>> torch.cuda.is_available()
+True
+>>> 
+"""
+
+#
+
+- IMAGE_NET http://imagenet.stanford.edu
+- https://github.com/pytorch/vision
+- AlexNet - http://mng.bz/lo6z
+- ReLU was introduced in this paper by Vinod Nair and Geoffrey E. Hinton in 2010.		https://towardsdatascience.com/what-alexnet-brought-to-the-world-of-deep-learning-46c7974b46fc
+- ResNET - Residual Network -- https://arxiv.org/pdf/1512.03385.pdf
+
+
+#
+
+- pip install torchvision - https://github.com/pytorch/vision
+
+#
+
+
+
+
