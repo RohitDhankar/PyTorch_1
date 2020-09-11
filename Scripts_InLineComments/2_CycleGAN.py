9ce2ca8d650914b6de060361a2c03b6610240f6a
RohitDhankar <dhankar.rohit@gmail.com>
2020-09-11 08:53:45 +0530

    CNN_ScreenCaptures

---
+++ Scripts_InLineComments/2_CycleGAN.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class ResNetBlock(nn.Module): # <1>
+    def __init__(self, dim):
+        super(ResNetBlock, self).__init__()
+        self.conv_block = self.build_conv_block(dim)
+
+    def build_conv_block(self, dim):
+        conv_block = []
+        conv_block += [nn.ReflectionPad2d(1)]
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=0, bias=True),
+                       nn.InstanceNorm2d(dim),
+                       nn.ReLU(True)]
+        conv_block += [nn.ReflectionPad2d(1)]
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=0, bias=True),
+                       nn.InstanceNorm2d(dim)]
+        return nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        out = x + self.conv_block(x) # <2>
+        return out
