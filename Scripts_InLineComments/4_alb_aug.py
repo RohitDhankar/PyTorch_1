655bba02d6c0530246c211c3fa1a791710e4ee0a
RohitDhankar <dhankar.rohit@gmail.com>
2020-09-11 17:29:42 +0530

    albumentations

--- Scripts_InLineComments/4_alb_aug.py
+++ Scripts_InLineComments/4_alb_aug.py
@@ -20,5 +20,19 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 #vis_img(image)    
 transform = alb.HorizontalFlip(p=0.5)
 hflip_img = transform(image=image)['image']
-vis_img(hflip_img)
+#vis_img(hflip_img)
+
+alb_transform = alb.Compose([
+    alb.CLAHE(),
+    alb.RandomRotate90(),
+    alb.Transpose(),
+    alb.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.50, rotate_limit=45, p=.75),
+    alb.Blur(blur_limit=3),
+    alb.OpticalDistortion(),
+    alb.GridDistortion(),
+    alb.HueSaturationValue(),
+])
+
+alb_aug_img = alb_transform(image=image)['image']
+vis_img(alb_aug_img)
 
